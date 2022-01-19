6b8eb0b1755ab554e752a95fcc9698023f909557
Vixx_ <40926097+Vixx-X@users.noreply.github.com>
2022-01-19 19:18:24 -0400

    chore: replace ugettext_lazy with gettext_lazy
    Replace ugettext_lazy with gettext_lazy, due to ugettext_lazy been deprecated in 3.0+.
    
    See https://code.djangoproject.com/ticket/30165

--- simple_mail/models.py
+++ simple_mail/models.py
@@ -6,7 +6,7 @@ from django.db import models
 from django.conf import settings
 from django.template import (Context, Template, loader)
 from django.core.mail import EmailMultiAlternatives, get_connection
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.core.files.storage import get_storage_class
 
 from pilkit.processors import ResizeToFit
